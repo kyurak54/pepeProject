0c16710cea188a6a9c7a0be99877e2610c3973ad
kyurak <kyurak54@gmail.com>
2022-06-27 18:37:06 +0900

    계산기 pj 시작

--- app/src/main/java/com/example/pepeproject/MainActivity.kt
+++ app/src/main/java/com/example/pepeproject/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.pepeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewParent
+import android.widget.AdapterView
+import android.widget.*
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Spinner에 들어갈 데이터
+        var data = listOf("-- 선택하세요 -- ", "선택 항목 1", "선택 항목 2", "선택 항목 3")
+
+        // 데이터와 스피터를 연결 시켜줄 adapter를 만들어 준다.
+        // ArrayAdapter의 두번째 인자는 스피너 목록에 아이템을 그려줄 레이아웃을 지정해준다.
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        // activity_main에서 만들어 놓은 spinner에 adapter를 연결해준다.
+
+        spinner.adapter = adapter
+        // 데이터가 들어가 있는 spinner에서 선택한 아이템을 가져온다.
+        spinner.onItemSelectListner = object:AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // position은 선택한 아이템의 위치를 넘겨주는 인자다
+                result.text = data.get(position)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
     }
+
 }
